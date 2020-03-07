7fc9e356e6caec6d60b779fc078bac00819e2e42
int255 <int255@outlook.com>
2020-03-07 16:04:27 +0800

    Update Widget.h

--- practice/pimp/Widget.h
+++ practice/pimp/Widget.h
@@ -4,12 +4,15 @@ class Widget
 {
 public:
 	Widget();
+
+	// pimp with via unique_ptr
+	// must declared dtor()
+	// otherwise compiler will generate default dtor, which will call default dtor for unique_ptr, which implies calling delete WidgetImp.
 	~Widget();
 	
 	const char * name() const;
 private:
 	struct WidgetImp;
 	std::unique_ptr<WidgetImp> _pimp;
-	//std::shared_ptr<WidgetImp> _pimp;
 	
 };
